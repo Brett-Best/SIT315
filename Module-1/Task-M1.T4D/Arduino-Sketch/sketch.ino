ae4dd57ecdfca5de8045e020da9b281ac920a3e3
Brett Best <brettbest@live.com.au>
2019-03-23 13:02:40 +1100

    Added code to detect multiple pins.

--- Module-1/Task-M1.T4D/Arduino-Sketch/sketch.ino
+++ Module-1/Task-M1.T4D/Arduino-Sketch/sketch.ino
@@ -3,38 +3,55 @@
 static const int sBaudRate = 9600;
 
 enum Pin {
+  LED_BOARD = 13,
   LED_RED = 12,
   LED_BLUE = 11,
   PIR1 = 2,
-  PIR2 = 3
+  PIR2 = 3,
+  PIR3 = 4,
+  PIR4 = 5,
+  PIR5 = 6,
+  PIR6 = 7,
+  PIR7 = 8,
+  PIR8 = 9,
+  PIR9 = 10
 };
 
-bool motionDetected[2] = {false, false};
+struct PIR {
+  int pin;
+};
+
+PIR monitoredPIRs[9] = {};
+
+void pinChanged(PIR *pinWrapped, bool pinState);
 
 void setup()
 {
   Serial.begin(sBaudRate);
 
+  pinMode(Pin::LED_BOARD, OUTPUT);
   pinMode(Pin::LED_RED, OUTPUT);
   pinMode(Pin::LED_BLUE, OUTPUT);
-  pinMode(Pin::PIR1, INPUT);
-  pinMode(Pin::PIR2, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(Pin::PIR1), toggleLED_RED, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(Pin::PIR2), toggleLED_BLUE, CHANGE);
+  digitalWrite(Pin::LED_BOARD, LOW);
+
+  for(int pin = Pin::PIR1; pin <= Pin::PIR9; pin++)
+  {
+    pinMode(pin, INPUT_PULLUP);
+    monitoredPIRs[pin-Pin::PIR1] = { pin };
+    PcInt::attachInterrupt(pin, pinChanged, &monitoredPIRs[pin-Pin::PIR1], CHANGE);
+  }
 }
 
 void loop() {}
 
-void toggleLED(int ledId) {
-  int LED = 0 == ledId ? Pin::LED_RED : Pin::LED_BLUE;
-  
-  motionDetected[ledId] = !motionDetected[ledId];
+void pinChanged(PIR *pir, bool pinState) {
+  int LED = 0 == pir->pin % 2 ? Pin::LED_RED : Pin::LED_BLUE;
   
   Serial.print("Led: ");
-  Serial.print(0 == ledId ? "Red" : "Blue");
+  Serial.print(0 == pir->pin % 2 ? "Red" : "Blue");
   Serial.print(", Motion Detected: ");
-  Serial.println(motionDetected[ledId]);
+  Serial.println(pir->pin % 2);
 
-  digitalWrite(LED, motionDetected[ledId] ? HIGH : LOW);
+  digitalWrite(LED, pinState ? HIGH : LOW);
 }
