55961cadcdde7ab9f0769e3cc7394bf3701cb9ae
Brett Best <brettbest@live.com.au>
2019-03-23 11:59:27 +1100

    Initial copy from 1.3C.

---
+++ Module-1/Task-M1.T4D/Arduino-Sketch/sketch.ino
@@ -0,0 +1,40 @@
+#include <YetAnotherPcInt.h>
+
+static const int sBaudRate = 9600;
+
+enum Pin {
+  LED_RED = 12,
+  LED_BLUE = 11,
+  PIR1 = 2,
+  PIR2 = 3
+};
+
+bool motionDetected[2] = {false, false};
+
+void setup()
+{
+  Serial.begin(sBaudRate);
+
+  pinMode(Pin::LED_RED, OUTPUT);
+  pinMode(Pin::LED_BLUE, OUTPUT);
+  pinMode(Pin::PIR1, INPUT);
+  pinMode(Pin::PIR2, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(Pin::PIR1), toggleLED_RED, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(Pin::PIR2), toggleLED_BLUE, CHANGE);
+}
+
+void loop() {}
+
+void toggleLED(int ledId) {
+  int LED = 0 == ledId ? Pin::LED_RED : Pin::LED_BLUE;
+  
+  motionDetected[ledId] = !motionDetected[ledId];
+  
+  Serial.print("Led: ");
+  Serial.print(0 == ledId ? "Red" : "Blue");
+  Serial.print(", Motion Detected: ");
+  Serial.println(motionDetected[ledId]);
+
+  digitalWrite(LED, motionDetected[ledId] ? HIGH : LOW);
+}
