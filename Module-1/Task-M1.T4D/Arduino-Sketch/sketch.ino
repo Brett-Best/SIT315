447cd859b91f51c5cd8523cc587ea23a604505eb
Brett Best <brettbest@live.com.au>
2019-03-23 14:09:05 +1100

    Added timer interrupt and updated the schematic.

--- Module-1/Task-M1.T4D/Arduino-Sketch/sketch.ino
+++ Module-1/Task-M1.T4D/Arduino-Sketch/sketch.ino
@@ -2,26 +2,24 @@
 
 static const int sBaudRate = 9600;
 
+const uint16_t tl_load = 0;
+const uint16_t tl_comp = 31250;
+
 enum Pin {
-  LED_BOARD = 13,
+  LED_BOARD = PB5,
   LED_RED = 12,
   LED_BLUE = 11,
   PIR1 = 2,
   PIR2 = 3,
   PIR3 = 4,
-  PIR4 = 5,
-  PIR5 = 6,
-  PIR6 = 7,
-  PIR7 = 8,
-  PIR8 = 9,
-  PIR9 = 10
+  PIR4 = 5
 };
 
 struct PIR {
   int pin;
 };
 
-PIR monitoredPIRs[9] = {};
+PIR monitoredPIRs[4] = {};
 
 void pinChanged(PIR *pinWrapped, bool pinState);
 
@@ -29,29 +27,59 @@ void setup()
 {
   Serial.begin(sBaudRate);
 
-  pinMode(Pin::LED_BOARD, OUTPUT);
   pinMode(Pin::LED_RED, OUTPUT);
   pinMode(Pin::LED_BLUE, OUTPUT);
 
-  digitalWrite(Pin::LED_BOARD, LOW);
-
-  for(int pin = Pin::PIR1; pin <= Pin::PIR9; pin++)
+  for (int pin = Pin::PIR1; pin <= Pin::PIR4; pin++)
   {
     pinMode(pin, INPUT_PULLUP);
     monitoredPIRs[pin-Pin::PIR1] = { pin };
     PcInt::attachInterrupt(pin, pinChanged, &monitoredPIRs[pin-Pin::PIR1], CHANGE);
   }
+
+  // Set the onboard led pin to OUTPUT
+  DDRB |= (1 << Pin::LED_BOARD);
+
+  // Reset Timer1 Control Register
+  TCCR1A = 0;
+
+  // CTC mode configuration
+  TCCR1B &= (1 << WGM13);
+  TCCR1B |= (1 << WGM12);
+
+  // Set to prescaler 1024
+  TCCR1B |= (1 << CS12);
+  TCCR1B &= ~(1 << CS11);
+  TCCR1B |= (1 << CS10);
+
+  // Reset Timer1 and set the compare value
+  TCNT1 = tl_load;
+  OCR1A = tl_comp;
+
+  // Enable Timer1 compare interrupt
+  TIMSK1 = (1 << OCIE1A);
+
+  // Enable global interrupts
+  sei();
 }
 
-void loop() {}
+void loop() {
+}
+
+ISR(TIMER1_COMPA_vect) {
+  Serial.println("Timer Fired");
+  PORTB ^= (1 << Pin::LED_BOARD);
+}
 
 void pinChanged(PIR *pir, bool pinState) {
   int LED = 0 == pir->pin % 2 ? Pin::LED_RED : Pin::LED_BLUE;
   
   Serial.print("Led: ");
   Serial.print(0 == pir->pin % 2 ? "Red" : "Blue");
+  Serial.print(", Pin: ");
+  Serial.print(pir->pin);
   Serial.print(", Motion Detected: ");
-  Serial.println(pir->pin % 2);
+  Serial.println(pinState);
 
   digitalWrite(LED, pinState ? HIGH : LOW);
 }
