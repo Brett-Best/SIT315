3ee6a69490fda3d6381009c15135c69608bd1cce
Brett Best <brettbest@live.com.au>
2019-04-09 03:33:21 +1000

    Added matrix multiplication xcode projects.

---
+++ Module-2/Task-M2.T1P/Matrix Multiplication - OpenMP/Matrix Multiplication - OpenMP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Matrix Multiplication - OpenMP
+//
+//  Created by Brett Best on 9/4/19.
+//  Copyright © 2019 Brett Best. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
