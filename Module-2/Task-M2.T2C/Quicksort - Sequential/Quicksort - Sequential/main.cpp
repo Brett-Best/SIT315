1fe8937eca3a3942ce3778d717015d7169e44687
Brett Best <brettbest@live.com.au>
2019-04-23 02:54:09 +1000

    Create Xcode workspace and projects for 2.2

---
+++ Module-2/Task-M2.T2C/Quicksort - Sequential/Quicksort - Sequential/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Quicksort - Sequential
+//
+//  Created by Brett Best on 23/4/19.
+//  Copyright © 2019 Brett Best. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
