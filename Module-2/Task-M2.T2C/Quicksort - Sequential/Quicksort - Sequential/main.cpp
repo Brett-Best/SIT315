816c5d0cbb5b83b7a3c857d07dca112771f17807
Brett Best <brettbest@live.com.au>
2019-04-23 04:39:47 +1000

    Add sequential version of quick sort.

--- Module-2/Task-M2.T2C/Quicksort - Sequential/Quicksort - Sequential/main.cpp
+++ Module-2/Task-M2.T2C/Quicksort - Sequential/Quicksort - Sequential/main.cpp
@@ -7,9 +7,79 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+struct timespec startTimespec, endTimespec;
+
+void startTimer() {
+  clock_gettime(CLOCK_MONOTONIC, &startTimespec);
+}
+
+void stopTimer() {
+  clock_gettime(CLOCK_MONOTONIC, &endTimespec);
+}
+
+vector<int> quicksort(vector<int> intVector) {
+  if (intVector.size() > 1 == false) {
+    return intVector;
+  }
+  
+  int pivot = intVector[intVector.size()/2];
+  
+  vector<int> less, equal, greater;
+  
+  copy_if(intVector.begin(), intVector.end(), back_inserter(less), [&](int i) { return i < pivot; });
+  copy_if(intVector.begin(), intVector.end(), back_inserter(equal), [&](int i) { return i == pivot; });
+  copy_if(intVector.begin(), intVector.end(), back_inserter(greater), [&](int i) { return i > pivot; });
+ 
+  vector<int> lessSorted = quicksort(less);
+  vector<int> moreSorted = quicksort(greater);
+  
+  vector<int> allSorted;
+  
+  copy(lessSorted.begin(), lessSorted.end(), back_inserter(allSorted));
+  copy(equal.begin(), equal.end(), back_inserter(allSorted));
+  copy(moreSorted.begin(), moreSorted.end(), back_inserter(allSorted));
+  
+  return allSorted;
+}
+
+double durationBetweenTimers() {
+  double seconds = (endTimespec.tv_sec - startTimespec.tv_sec);
+  seconds += (endTimespec.tv_nsec - startTimespec.tv_nsec) / 1000000000.0;
+  
+  return seconds;
+}
+
+vector<int> randomIntVector(int length) {
+  vector<int> v(length);
+  
+  for (int index = 0; index < v.size(); index++) {
+    v[index] = rand() % 1'000'000;
+  }
+  
+  return v;
+}
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
+  startTimer();
+  
+  vector<int> v = randomIntVector(10000000);
+  
+  stopTimer();
+  
+  printf("Generate Vector: %.3fs\n", durationBetweenTimers());
+  
+  startTimer();
+  
+  vector<int> vSorted = quicksort(v);
+  
+  stopTimer();
+  
+  printf("Sort Vectors: %.3fs\n", durationBetweenTimers());
+  
   return 0;
 }
