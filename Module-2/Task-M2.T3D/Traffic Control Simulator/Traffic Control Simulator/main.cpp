3882d7aa57299da5ce40fa414eb32139f12fd3db
Brett Best <brettbest@live.com.au>
2019-05-10 11:09:42 +1000

    Read from CSV file

--- Module-2/Task-M2.T3D/Traffic Control Simulator/Traffic Control Simulator/main.cpp
+++ Module-2/Task-M2.T3D/Traffic Control Simulator/Traffic Control Simulator/main.cpp
@@ -7,9 +7,59 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+struct TrafficSignalEntry {
+  long long timestamp;
+  int id;
+  int numberOfCars;
+};
+
+void readCSV(const string &csvPath) {
+  ifstream csvFile;
+  csvFile.open(csvPath.c_str());
+  
+  if (!csvFile.is_open()) {
+    printf("Path is wrong!");
+    exit(EXIT_FAILURE);
+  }
+  
+  vector<TrafficSignalEntry> trafficSignalEntries;
+  
+  string line;
+  vector <string> vec;
+  getline(csvFile, line); // Skip header row
+  
+  while (getline(csvFile,line)) {
+    if (line.empty()) {
+      continue; // Skip empty lines
+    }
+    
+    istringstream iStringStream(line);
+    string lineStream;
+    string::size_type sz;
+    
+    vector <long long> row;
+    
+    while (getline(iStringStream, lineStream, ','))
+    {
+      row.push_back(stoll(lineStream,&sz));
+    }
+    
+    TrafficSignalEntry trafficSignalEntry = TrafficSignalEntry { row[0], (int)row[1], (int)row[2] };
+    trafficSignalEntries.push_back(trafficSignalEntry);
+  }
+  
+}
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
+  readCSV("trafficData.csv");
+  
   return 0;
 }
